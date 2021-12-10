275e59cc381b5435ce1fafa2ab7b17444931036e
Viktor Nagy <viktor.nagy@01people.com>
2021-12-10 12:17:11 +0100

    feat: first working demo

--- lua/typebreak/init.lua
+++ lua/typebreak/init.lua
@@ -4,29 +4,31 @@ local api = vim.api
 local curl = require("plenary.curl")
 
 M.buf = nil
+M.memory = ""
+M.words = {}
+M.lines = {}
+M.width = 50
+M.height = 11
 
 function M.start()
-    local width = 50
-    local height = 11
     local ui = api.nvim_list_uis()[1]
 
     local response = curl.get("https://random-word-api.herokuapp.com/word?number=10")
     local body = response.body
 
     local lines = {}
-    local words = {}
 
     local delimiter = ","
     for match in (body..delimiter):gmatch("(.-)"..delimiter) do
         match = string.gsub(match, '%W', '')
-        table.insert(words, match)
+        table.insert(M.words, match)
     end
 
-    for _, word in pairs(words) do
+    for _, word in pairs(M.words) do
         local length = string.len(word)
-        local before = math.random(0, width-length)
-        local after = width - length - before
-        table.insert(lines, string.rep(' ', before) .. word .. string.rep(' ', after))
+        local before = math.random(0, M.width-length)
+        local after = M.width - length - before
+        table.insert(M.lines, string.rep(' ', before) .. word .. string.rep(' ', after))
     end
 
     local buf = api.nvim_create_buf(false, 0)
@@ -35,23 +37,38 @@ function M.start()
 
     local opts = {
         relative = "editor",
-        width = width,
-        height = height,
-        col = (ui.width/2) - (width/2),
-        row = (ui.height/2) - (height/2),
+        width = M.width,
+        height = M.height,
+        col = (ui.width/2) - (M.width/2),
+        row = (ui.height/2) - (M.height/2),
         border = "shadow",
         anchor = 'NW',
         style = 'minimal',
     }
-    api.nvim_buf_set_lines(buf, 0, 0, true, lines)
+    M.draw()
 
-    local win = api.nvim_open_win(buf, 1, opts)
+    api.nvim_open_win(buf, 1, opts)
 
     M.set_mapping()
 end
 
+function M.draw()
+    api.nvim_buf_set_lines(M.buf, 0, 10, false, M.lines)
+end
+
 function M.key_pressed(key)
-    print('PRESSED KEY' .. key)
+    M.memory = M.memory .. key
+    local match = false
+    for k, word in pairs(M.words) do
+        if string.sub(M.memory, -string.len(word)) == word then
+            match = true
+            M.lines[k] = string.rep(' ', M.width)
+        end
+    end
+    if match == true then
+        M.memory = ""
+        M.draw()
+    end
 end
 
 function M.set_mapping()
